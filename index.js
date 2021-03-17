650f9099b73cd1bab6eb27fbcc063b24e3228ef6
Vladislav Kostic <vladislavkostic@yahoo.com>
2021-03-17 22:44:10 +0100

    Nested Components
    Using nested components in React.

---
+++ index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+
+
+function Greeting(){
+  return( 
+  <div>
+    <Person/>
+    <Message/>
+  </div>
+  );
+}
+
+const Person = () => <h2>John Doe</h2>;
+const Message = () => {
+  return <p>This is my message!</p>;
+};
+
+ReactDom.render(<Greeting/>, document.getElementById('root')
+);
