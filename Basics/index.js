64242d9e781b01ba0d6169702bbc7b51cdc9977e
Vladislav Kostic <vladislavkostic@yahoo.com>
2021-03-17 23:17:13 +0100

    Book List
    Simple book list. It's the same book but mainly it's purpose is instructive.

---
+++ Basics/index.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import './index.css'  
+
+function BookList(){
+  return( 
+  <section className = 'booklist'> 
+    <Book/>   
+    <Book/>
+    <Book/>
+    <Book/>
+    <Book/>
+    <Book/>
+    <Book/>
+    <Book/>
+    <Book/>
+    <Book/>
+  </section>
+  );
+}
+
+const Book = () => {
+  return <article className = 'book'>
+    <Image/>
+    <Title/>
+    <Author/>
+  </article>
+}
+
+const Image = () => <img src="https://images-na.
+ssl-images-amazon.com/images/I/71xLmdLOQ0L.
+_AC_UL200_SR200,200_.jpg" alt="I here am not one destroyed piece of shit"/>
+
+const Title = () => <h1>12 More Rules for Life</h1>
+const Author = () => <h4>Jordan Peterson</h4>
+
+ReactDom.render(<BookList/>, document.getElementById('root')
+);
