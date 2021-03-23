35aff319b2a1c0e5256a033e249bf141324ed42a
Vladislav Kostic <vladislavkostic@yahoo.com>
2021-03-23 22:25:53 +0100

    Book List
    Component which stores all the books that are displayed.

---
+++ Basics/Simple Project/src/Books.js
@@ -0,0 +1,22 @@
+export const books = [
+  {
+    id: 1,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/71xLmdLOQ0L._AC_UL200_SR200,200_.jpg',
+    title: '12 More Rules for Life',
+    author: 'Jordan Peterson',
+  } 
+  ,
+  {
+    id: 2,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81WZ6QvGZ2L._AC_UL200_SR200,200_.jpg',
+    title: "It's Not Easy Being a Bunny",
+    author: 'Marilyn Sadler',
+  } 
+  ,
+  {
+    id: 3,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81kdwZtIGbL._AC_UL200_SR200,200_.jpg',
+    title: 'Green Eggs and Ham',
+    author: 'Dr. Seuss',
+  } 
+]
